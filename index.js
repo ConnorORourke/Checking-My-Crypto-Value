db796336c7690f08efd8631fcc6d713fdb8704aa
Connor O'Rourke <connororourke101@gmail.com>
2021-02-05 11:59:12 +0000

    updated port

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.get("/:name", async (req, res) => {
   return res.send("Name not recognised");
 });
 
-const server = app.listen(8080, () => {
+const server = app.listen(process.env.PORT || 8080, () => {
   const host = server.address().address;
   const port = server.address().port;
   console.log(`App listening at http://${host}:${port}`);
